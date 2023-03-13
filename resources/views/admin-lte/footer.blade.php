59a8b87ffe5580c1be4248c4a4de582eb11acab6
KalysaNaomiP14 <kalysanaomi62@gmail.com>
2023-03-13 14:59:32 +0700

    first commit

---
+++ resources/views/admin-lte/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="row justify-content-center">
+      <strong>Kalysa Naomi Putri | XII RPL 4 | SMK Telkom Purwokerto</strong>
+    </div>
+</footer>
---
+++ resources/views/admin-lte/preloader.blade.php
@@ -0,0 +1,3 @@
+ <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="/adminlte/dist/img/book1.png" alt="book1" height="60" width="60">
+  </div>
